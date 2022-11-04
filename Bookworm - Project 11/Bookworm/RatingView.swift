5501b75238e1a2984e18d89f0b5e43cdeb52a5bb
Eric Tolson <91225947+erictolson@users.noreply.github.com>
2022-11-04 10:50:00 -0700

    added accessibility to star rating feature

--- Bookworm - Project 11/Bookworm/RatingView.swift
+++ Bookworm - Project 11/Bookworm/RatingView.swift
@@ -29,10 +29,19 @@ struct RatingView: View {
 
             ForEach(1..<maximumRating + 1, id: \.self) { number in
                 image(for: number)
-                    .foregroundColor(number > rating ? offColor : onColor)
-                    .onTapGesture {
-                        rating = number
-                    }
+            }
+        }
+        .accessibilityElement()
+        .accessibilityLabel(label)
+        .accessibilityValue(rating == 1 ? "1 star" : "\(rating) stars")
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                if rating < maximumRating { rating += 1 }
+            case .decrement:
+                if rating > 1 { rating -= 1 }
+            default:
+                break
             }
         }
     }
