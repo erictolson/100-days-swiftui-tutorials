df4cb96e356b1df404720f97be5f894d362847f5
Eric Tolson <91225947+erictolson@users.noreply.github.com>
2022-11-04 10:53:01 -0700

    added accessibility modifiers to word submissions

--- WordScramble - Project 5/WordScramble/ContentView.swift
+++ WordScramble - Project 5/WordScramble/ContentView.swift
@@ -33,6 +33,9 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
+                        .accessibilityElement(children: .ignore)
+                        .accessibilityLabel(word)
+                        .accessibilityHint("\(word.count) letters")   
                     }
                 }
                 Section ("Score") {
